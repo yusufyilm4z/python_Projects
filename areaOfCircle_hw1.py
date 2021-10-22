0ca518379372bfc24d2de0064397e610b86fb16b
yusufylmz <yusufylm4z@gmail.com>
2021-10-22 18:39:06 +0300

    main

---
+++ areaOfCircle_hw1.py
@@ -0,0 +1,4 @@
+r=input("Please enter your circles radius: ")
+pi=3.14
+areaOfCircle=pi*(int(r)**2)
+print(areaOfCircle)
---
+++ main4.py
@@ -0,0 +1,7 @@
+import math
+radius = int(input("please write the radius of circle: "))
+pi=3.14
+areaOfCircle= (radius**2) * pi
+area2= math.pow(radius,2) * math.pi
+print("Area of your circle is: ",areaOfCircle, area2)
+
